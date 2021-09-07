647eb3ca8228a003a835d3ff88e03e8b6f4aa871
Tayyabali <tayyabsayyad@gmail.com>
2021-09-07 15:11:01 +0530

    Message Displayed

--- src/Example.java
+++ src/Example.java
@@ -4,11 +4,12 @@ import javax.sound.midi.Soundbank;
     tHIS IS MY FIRST jAVA pROGRAM
  */
 class Example {
-    public static void main(String args[])
+    public static void main(String[] args)
     {
         int num = 10;
         System.out.println(" Num = "+num);
         num = 2 * num;
+        System.out.println("Value of num is multiplied by 2 ");
         System.out.println(num);
     }
 }
