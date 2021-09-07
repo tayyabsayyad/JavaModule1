4c3c9633f230154986a302db5241f5e7c7566533
Tayyabali <tayyabsayyad@gmail.com>
2021-09-07 15:08:00 +0530

    Variables added

--- src/Example.java
+++ src/Example.java
@@ -1,4 +1,14 @@
-public class Example {
+import javax.sound.midi.Soundbank;
+
+/*
+    tHIS IS MY FIRST jAVA pROGRAM
+ */
+class Example {
+    public static void main(String args[])
+    {
+        int num = 10;
+        System.out.println(" Num = "+num);
+        num = 2 * num;
+        System.out.println(num);
+    }
 }
-
-
